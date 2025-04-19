3af5e79a0c127e243b6e264d1272a57bd000a49d
Suraj370 <surajpanda2077@gmail.com>
2025-04-19 10:24:17 +0530

    project setup

---
+++ reviews-ms/src/main/java/com/movie/ReviewsMsApplication.java
@@ -0,0 +1,13 @@
+package com.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewsMsApplication.class, args);
+	}
+
+}
